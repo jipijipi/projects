0b25456ab721af7e4c21cd5b932387b195662602
jipijipi <jp.leflot@gmail.com>
2020-10-23 11:57:28 +0200

    added modules and board drawing

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -9,10 +9,11 @@ let gameBoard = (() => {
         board.push(subArray);
     }
 
-    return board;
+    return { board, boardSize };
 })();
 
-gameBoard = [
+//test case
+gameBoard.board = [
     [1, 1, 2],
     [1, 2, 1],
     [2, 0, 1],
@@ -20,50 +21,73 @@ gameBoard = [
 
 //check solutions
 
-function check(arr) {
-    return arr.every((x) => x > 0 && x == arr[0]) ? arr[0] : false;
-}
+let game = (() => {
+    function check(arr) {
+        return arr.every((x) => x > 0 && x == arr[0]) ? arr[0] : false;
+    }
 
-function checkArray() {
-    const resultsTable = [];
-    let boardSize = gameBoard[0].length;
+    let checkArray = () => {
+        const resultsTable = [];
+        let boardSize = gameBoard.boardSize;
+        let board = gameBoard.board;
 
-    //rows
-    let rows = gameBoard.forEach((x) => resultsTable.push(x));
+        console.log({ boardSize, board });
 
-    //columns
-    let columnArray = [];
+        //rows
+        let rows = board.forEach((x) => resultsTable.push(x));
 
-    for (let i = 0; i < boardSize; i++) {
-        let subArray = [];
-        for (let j = 0; j < boardSize; j++) {
-            subArray.push(gameBoard[j][i]);
+        //columns
+        let columnArray = [];
+
+        for (let i = 0; i < boardSize; i++) {
+            let subArray = [];
+            for (let j = 0; j < boardSize; j++) {
+                subArray.push(board[j][i]);
+            }
+
+            columnArray.push(subArray);
         }
 
-        columnArray.push(subArray);
-    }
+        columnArray.forEach((x) => resultsTable.push(x));
 
-    columnArray.forEach((x) => resultsTable.push(x));
-
-    //first diagonal
-    let firstDiagonalArray = [];
-    for (i = 0; i < boardSize; i++) {
-        firstDiagonalArray.push(gameBoard[i][i]);
-    }
-    resultsTable.push(firstDiagonalArray);
-
-    //second diagonal
-    let secondDiagonalArray = [];
-    for (i = 0; i < boardSize; i++) {
-        secondDiagonalArray.push(gameBoard[i][boardSize - 1 - i]);
-    }
-    resultsTable.push(secondDiagonalArray);
-
-    //check
-    console.log(resultsTable);
-    for (let item of resultsTable) {
-        if (check(item)) {
-            return check(item) + ' wins !';
+        //first diagonal
+        let firstDiagonalArray = [];
+        for (i = 0; i < boardSize; i++) {
+            firstDiagonalArray.push(board[i][i]);
         }
+        resultsTable.push(firstDiagonalArray);
+
+        //second diagonal
+        let secondDiagonalArray = [];
+        for (i = 0; i < boardSize; i++) {
+            secondDiagonalArray.push(board[i][boardSize - 1 - i]);
+        }
+        resultsTable.push(secondDiagonalArray);
+
+        //check
+        console.log(resultsTable);
+        for (let item of resultsTable) {
+            if (check(item)) {
+                return check(item) + ' wins !';
+            }
+        }
+    };
+
+    return { checkArray };
+})();
+
+// game display
+
+let drawBoard = () => {
+    numberOfSquares = gameBoard.boardSize ** 2;
+    let gameSpace = document.querySelector('#game');
+
+    for (let i = 0; i < numberOfSquares; i++) {
+        let square = document.createElement('button');
+        square.className = 'square';
+        square.innerText = '.';
+        gameSpace.append(square);
     }
-}
+};
+
+drawBoard();
