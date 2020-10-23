918272c806248aee474e3cda214118a216ba0205
jipijipi <jp.leflot@gmail.com>
2020-10-23 03:54:58 +0200

    working solver

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,11 +1,11 @@
 //make initial board
 
 let gameBoard = (() => {
-    let boardsize = 3;
+    let boardSize = 3;
     const board = [];
 
-    for (let i = 0; i < boardsize; i++) {
-        let subArray = new Array(boardsize).fill(0);
+    for (let i = 0; i < boardSize; i++) {
+        let subArray = new Array(boardSize).fill(0);
         board.push(subArray);
     }
 
@@ -13,9 +13,9 @@ let gameBoard = (() => {
 })();
 
 gameBoard = [
-    [1, 0, 1],
-    [0, 1, 1],
-    [0, 0, 1],
+    [1, 1, 2],
+    [1, 2, 1],
+    [2, 0, 1],
 ];
 
 //check solutions
@@ -26,8 +26,44 @@ function check(arr) {
 
 function checkArray() {
     const resultsTable = [];
+    let boardSize = gameBoard[0].length;
 
+    //rows
     let rows = gameBoard.forEach((x) => resultsTable.push(x));
 
-    return resultsTable;
+    //columns
+    let columnArray = [];
+
+    for (let i = 0; i < boardSize; i++) {
+        let subArray = [];
+        for (let j = 0; j < boardSize; j++) {
+            subArray.push(gameBoard[j][i]);
+        }
+
+        columnArray.push(subArray);
+    }
+
+    columnArray.forEach((x) => resultsTable.push(x));
+
+    //first diagonal
+    let firstDiagonalArray = [];
+    for (i = 0; i < boardSize; i++) {
+        firstDiagonalArray.push(gameBoard[i][i]);
+    }
+    resultsTable.push(firstDiagonalArray);
+
+    //second diagonal
+    let secondDiagonalArray = [];
+    for (i = 0; i < boardSize; i++) {
+        secondDiagonalArray.push(gameBoard[i][boardSize - 1 - i]);
+    }
+    resultsTable.push(secondDiagonalArray);
+
+    //check
+    console.log(resultsTable);
+    for (let item of resultsTable) {
+        if (check(item)) {
+            return check(item) + ' wins !';
+        }
+    }
 }
