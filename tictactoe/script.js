8ccce3d167611ab0a516feaf6dc7637580365fd0
jipijipi <jp.leflot@gmail.com>
2020-10-23 02:02:53 +0200

    started checkArray

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,8 +1,8 @@
-let board = makeBoard();
+//make initial board
 
-function makeBoard() {
+let gameBoard = (() => {
     let boardsize = 3;
-    let board = [];
+    const board = [];
 
     for (let i = 0; i < boardsize; i++) {
         let subArray = new Array(boardsize).fill(0);
@@ -10,8 +10,24 @@ function makeBoard() {
     }
 
     return board;
+})();
+
+gameBoard = [
+    [1, 0, 1],
+    [0, 1, 1],
+    [0, 0, 1],
+];
+
+//check solutions
+
+function check(arr) {
+    return arr.every((x) => x > 0 && x == arr[0]) ? arr[0] : false;
 }
 
-function checkRow(arr) {
-    return arr.every((x) => x > 0 && x == arr[0]) ? arr[0] : false;
+function checkArray() {
+    const resultsTable = [];
+
+    let rows = gameBoard.forEach((x) => resultsTable.push(x));
+
+    return resultsTable;
 }
