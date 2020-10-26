64fee86b16f618e27e0f5b1862754f3e690cf35d
jipijipi <jp.leflot@gmail.com>
2020-10-27 00:22:09 +0100

    added linter

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,19 +1,19 @@
-//make initial board
+// make initial board
 
-let gameBoard = (() => {
-    let boardSize = 3;
-    let board = [];
+const gameBoard = (() => {
+    const boardSize = 3;
+    const board = [];
 
     function drawBoard() {
         board.length = 0;
-        let gameSpace = document.querySelector('#game');
+        const gameSpace = document.querySelector('#game');
         gameSpace.innerHTML = '';
 
         for (let i = 0; i < boardSize; i++) {
-            let rowArr = [];
+            const rowArr = [];
             for (let j = 0; j < boardSize; j++) {
                 rowArr.push(0);
-                let square = document.createElement('button');
+                const square = document.createElement('button');
                 square.className = 'square';
                 square.setAttribute('row', i);
                 square.setAttribute('column', j);
@@ -31,35 +31,26 @@ let gameBoard = (() => {
     return { board, boardSize };
 })();
 
-//test case
-/* gameBoard.board = [
-    [1, 1, 2],
-    [1, 0, 1],
-    [2, 0, 1],
-]; */
-
-//check solutions
-
-let game = (() => {
+const game = (() => {
     function check(arr) {
         return arr.every((x) => x > 0 && x == arr[0]) ? arr[0] : false;
     }
 
-    let checkArray = () => {
+    const checkArray = () => {
         const resultsTable = [];
-        let boardSize = gameBoard.boardSize;
-        let board = gameBoard.board;
+        const { boardSize } = gameBoard;
+        const { board } = gameBoard;
 
         console.log({ boardSize, board });
 
-        //rows
-        let rows = board.forEach((x) => resultsTable.push(x));
+        // rows
+        // const rows = board.forEach((x) => resultsTable.push(x));
 
-        //columns
-        let columnArray = [];
+        // columns
+        const columnArray = [];
 
         for (let i = 0; i < boardSize; i++) {
-            let subArray = [];
+            const subArray = [];
             for (let j = 0; j < boardSize; j++) {
                 subArray.push(board[j][i]);
             }
@@ -69,25 +60,25 @@ let game = (() => {
 
         columnArray.forEach((x) => resultsTable.push(x));
 
-        //first diagonal
-        let firstDiagonalArray = [];
-        for (i = 0; i < boardSize; i++) {
+        // first diagonal
+        const firstDiagonalArray = [];
+        for (let i = 0; i < boardSize; i++) {
             firstDiagonalArray.push(board[i][i]);
         }
         resultsTable.push(firstDiagonalArray);
 
-        //second diagonal
-        let secondDiagonalArray = [];
-        for (i = 0; i < boardSize; i++) {
+        // second diagonal
+        const secondDiagonalArray = [];
+        for (let i = 0; i < boardSize; i++) {
             secondDiagonalArray.push(board[i][boardSize - 1 - i]);
         }
         resultsTable.push(secondDiagonalArray);
 
-        //check
+        // check
         console.log(resultsTable);
-        for (let item of resultsTable) {
+        for (const item of resultsTable) {
             if (check(item)) {
-                return check(item) + ' wins !';
+                return `${check(item)} wins !`;
             }
         }
     };
@@ -97,28 +88,28 @@ let game = (() => {
 
 // game display
 
-//player
+// player
 
-let playerOne = {
+const playerOne = {
     name: 1,
     color: '#ffdce2',
 };
-let playerTwo = {
+const playerTwo = {
     name: 2,
     color: '#c3e7e2',
 };
 
 let playerOneTurn = true;
 
-let currentPlayer = () => {
-    let name = playerOneTurn ? playerOne.name : playerTwo.name;
-    let color = playerOneTurn ? playerOne.color : playerTwo.color;
+const currentPlayer = () => {
+    const name = playerOneTurn ? playerOne.name : playerTwo.name;
+    const color = playerOneTurn ? playerOne.color : playerTwo.color;
     return { name, color };
 };
 
 function updateSquare() {
-    let x = this.getAttribute('row');
-    let y = this.getAttribute('column');
+    const x = this.getAttribute('row');
+    const y = this.getAttribute('column');
 
     if (gameBoard.board[x][y] === 0) {
         gameBoard.board[x][y] = currentPlayer().name;
