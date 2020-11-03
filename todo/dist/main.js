7875aafb2e10981b976afea487510683fcdd2c6f
jipijipi <jp.leflot@gmail.com>
2020-11-03 01:31:38 +0100

    fixed most things

--- todo/dist/main.js
+++ todo/dist/main.js
@@ -40,7 +40,7 @@ function addProjects() {
     content.innerHTML = '';
 
     let projectList = Object.entries(projects);
-    let projectNames = Object.keys(projects);
+    // let projectNames = Object.keys(projects);
 
     for (let [projectName, items] of projectList) {
         let itemBlock = document.createElement('ul');
@@ -48,18 +48,23 @@ function addProjects() {
         //create item block
         for (let item of items) {
             let itemLine = document.createElement('li');
+            itemLine.setAttribute('js-item-id', items.indexOf(item));
+            item['completed'] ? itemLine.classList.add('completed') : itemLine.classList.remove('completed');
 
             itemLine.append(item['description']);
+            itemLine.innerHTML += '<button class="js-delete-item">x</button>';
             itemBlock.append(itemLine);
         }
 
         //add input
-        itemBlock.append(document.createElement('input'));
+        let inputLine = document.createElement('li');
+        inputLine.innerHTML = '<input></input>';
+        itemBlock.append(inputLine);
 
         //create and populate projects
         let projectTemplate = `
         <div class="todo-wrapper" js-project-id="${projectName}">
-          <h3>${projectName}</h3>
+          <h3>${projectName}<button class="delete-project">X</button></h3>
           ${itemBlock.outerHTML}
         <button class="js-add-item">add</button>
         </div>`;
@@ -67,28 +72,14 @@ function addProjects() {
         content.insertAdjacentHTML('beforeend', projectTemplate);
     }
 
+    content.insertAdjacentHTML('beforeend', `<div class="break"></div><button class="add-project">New Project</button>`);
+
     addEvents();
 }
 
 addProjects();
 
-//find current project number
-// let getData = (() => {
-//     function currentProjectId() {
-//         let currentElement = this;
-//         console.log(this);
-//         let currentProject = currentElement.closest('div[js-project-id]');
-//         console.log(currentProject.getAttribute('js-project-id'));
-//         return currentProject.getAttribute('js-project-id');
-//     }
-//     return { currentProjectId };
-// })();
-
-// function inputToProjects() {
-//     let projectId = currentProjectId();
-//     let currentProject = document.querySelector(`div[js-project-id=${projectId}]`);
-//     console.log(currentProject);
-// }
+//get values
 
 function getInputValue(evt) {
     let inputValue = evt.target.closest('div[js-project-id]').querySelector('input').value;
@@ -100,20 +91,77 @@ function getProjectId(evt) {
     return projectId;
 }
 
-function putInputValue(evt) {
-    let inputValue = getInputValue(evt);
-    let projectId = getProjectId(evt);
-    projects[projectId].push({ description: inputValue });
-    addProjects();
-    console.log(inputValue, projectId, projects[projectId]);
+function getItemId(evt) {
+    let itemId = evt.target.closest('[js-item-id]').getAttribute('js-item-id');
+    return itemId;
 }
+
+//add project to projects
+
+function addProject() {
+    let projectName = prompt('Project Name?');
+    projects[projectName] = [];
+    addProjects();
+}
+
+//add item to project
+
+function putInputValue(evt) {
+    let description = getInputValue(evt);
+    let projectId = getProjectId(evt);
+    let completed = 0;
+    projects[projectId].push({ description, completed });
+    addProjects();
+}
+
+// delete project
+
+function deleteProject(evt) {
+    let projectId = getProjectId(evt);
+    delete projects[projectId];
+    addProjects();
+}
+
+function deleteItem(evt) {
+    let projectId = getProjectId(evt);
+    let itemId = getItemId(evt);
+    console.log(projects[projectId][itemId]);
+    projects[projectId].splice(itemId, 1);
+    evt.stopPropagation();
+
+    addProjects();
+}
+//toogle completed
+
+function toggleCompleted(evt) {
+    let projectId = getProjectId(evt);
+    let itemId = getItemId(evt);
+    let completedStatus = projects[projectId][itemId]['completed'];
+    completedStatus ? (projects[projectId][itemId]['completed'] = 0) : (projects[projectId][itemId]['completed'] = 1);
+    // evt.stopPropagation();
+
+    addProjects();
+}
+
 //events
 
 function addEvents() {
+    //add project
+    let addNewProject = document.querySelector('.add-project');
+    addNewProject.addEventListener('click', addProject);
+
     //add to projects
 
     let addButtons = document.querySelectorAll('.js-add-item');
     addButtons.forEach((x) => x.addEventListener('click', putInputValue));
+
+    //delete project
+    let deleteCurrentProject = document.querySelectorAll('.delete-project');
+    deleteCurrentProject.forEach((x) => x.addEventListener('click', deleteProject));
+
+    //delete item
+    let deleteCurrentItem = document.querySelectorAll('.js-delete-item');
+    deleteCurrentItem.forEach((x) => x.addEventListener('click', deleteItem));
 
     //strike out
 
@@ -122,9 +170,5 @@ function addEvents() {
     }
 
     let listItems = document.querySelectorAll('.todo-wrapper > ul > li');
-    listItems.forEach((x) => x.addEventListener('click', strikeItem));
-
-    //input
-
-    function createItem() {}
+    listItems.forEach((x) => x.addEventListener('click', toggleCompleted));
 }
