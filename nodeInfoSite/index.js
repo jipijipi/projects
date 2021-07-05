0c31cddf5b52066c0ea4c1a5bb0336826cb2f31e
jipijipi <jp.leflot@gmail.com>
2021-07-05 23:52:33 +0200

    fixed 404

--- nodeInfoSite/index.js
+++ nodeInfoSite/index.js
@@ -11,12 +11,16 @@ const requestListener = function (req, res) {
     fs.readFile(filename, function (err, data) {
         if (err) {
             console.log(err);
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end('404 Not Found');
+            fs.readFile('404.html', function (err, data) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
         }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
     });
 };
 
