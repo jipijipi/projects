911fcda703ab28d738c9f79376ca13befcb9b35b
JPL <jp.leflot@gmail.com>
2021-12-22 00:09:51 +0100

    output test

---
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8');
+
+console.log(textIn);
+
+const textOut = 'Heyo yayayaya';
+const newFile = './starter/txt/ouput.txt';
+
+fs.writeFileSync(newFile,textOut);
+console.log(`"${textOut}" has been written to path : ${newFile}`)
