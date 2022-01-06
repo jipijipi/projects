c384d5a62d5ebef9e4c8b733cb0889e33a01ed92
JPL <jp.leflot@gmail.com>
2022-01-06 17:50:56 +0100

    callback hell

--- complete-node-bootcamp-master/3-asynchronous-JS/index.js
+++ complete-node-bootcamp-master/3-asynchronous-JS/index.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = file => {
+    return new Promise();
+}
+
 fs.readFile(`${__dirname}/starter/dog.txt`, (err, data) => {
     console.log(`Breed : ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        console.log(res.body);
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res => {
+
+        if (err) return
+
+        console.log(res.body.message);
+
+        fs.writeFile('dog-img.txt', res.body.message, x => {
+            console.log('file created');
+        })
+
+    }).catch(err => {
+        console.log(err.message);
     });
 })
