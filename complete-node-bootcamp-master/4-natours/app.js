9a8c4b06aa7570b3a0478ce7d4925700fb1181ce
JPL <jp.leflot@gmail.com>
2022-01-17 17:05:58 +0100

    url params 54

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -23,9 +23,44 @@ app.get('/api/v1/tours', (req, res) => {
     });
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    console.log(req.params);
+
+    const id = + req.params['id'];
+    const tour = tours.find(el => el.id === id);
+
+    if (tour === undefined) {
+        return res.status(404).json({
+            status: 'not found bruh',
+        })
+    }
+
+    res.status(200).json({
+        status: 'succes',
+        data: { tour }
+    });
+})
+
 app.post('/api/v1/tours', (req, res) => {
     console.log(req.body);
-    res.status(200).send('done');
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            }
+        });
+
+    })
+
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
 
 })
 
